8ae138a8d2dd65055a2b06c3e956eac633f2c79f
Michael Li <dev.mike011235@gmail.com>
2020-07-19 13:25:28 -0400

    Cleaned formatting and added descriptive comments.

--- Develop/script.js
+++ Develop/script.js
@@ -3,39 +3,41 @@
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword()
-  // let passwordText = document.querySelector("#password")
-  // passwordText.value = password
-  $("#password").val(password)
+  let passwordText = document.querySelector("#password")
+  passwordText.value = password
+  // $("#password").val(password)
 }
 
 function generatePassword() {
   alert("Let's get you a new password!")
 
+  // User options
   let userPreferences = {
-    "length" : 0,
-    "lowercase" : false,
-    "uppercase" : false,
-    "numeric" : false,
-    "specialchars" : false
+    "length": 0,
+    "lowercase": false,
+    "uppercase": false,
+    "numeric": false,
+    "specialchars": false
   }
 
   userPreferences.length = chooseLength()
   userPreferences.lowercase = chooseLowercase()
-  userPreferences.uppercase = chooseUppercase() 
+  userPreferences.uppercase = chooseUppercase()
   userPreferences.numeric = chooseNumeric()
   userPreferences.specialchars = chooseSpecialChars()
 
   let oneCondition = (userPreferences.lowercase ||
-                      userPreferences.uppercase ||
-                      userPreferences.numeric ||
-                      userPreferences.specialchars)
+    userPreferences.uppercase ||
+    userPreferences.numeric ||
+    userPreferences.specialchars)
 
+  // Checks to see if the user selected at least one option.
   if (!oneCondition) {
     alert("You have to choose at least 1 condition in addition to the length of your password.")
     generatePassword()
   }
   else {
-    makePassword(userPreferences.length, userPreferences.lowercase, userPreferences.uppercase, userPreferences.numeric, userPreferences.specialchars)
+    return makePassword(userPreferences.length, userPreferences.lowercase, userPreferences.uppercase, userPreferences.numeric, userPreferences.specialchars)
   }
 
 }
@@ -49,76 +51,85 @@ function makePassword(pwLength, lowercase, uppercase, numeric, specialchars) {
   const chosenOptions = []
 
   if (uppercase) {
-      chosenOptions.push(upperCaseLetters)
+    chosenOptions.push(upperCaseLetters)
   }
 
   if (lowercase) {
-      chosenOptions.push(lowerCaseLetters)
+    chosenOptions.push(lowerCaseLetters)
   }
 
   if (numeric) {
-      chosenOptions.push(nums)
+    chosenOptions.push(nums)
   }
 
   if (specialchars) {
-      chosenOptions.push(specialCharacters)
+    chosenOptions.push(specialCharacters)
   }
 
+  // Stores random characters.
   let passwordArr = []
 
   for (let i = 0; i < pwLength; i++) {
-      // Pick a random array containing a rule the user chose. 
-      let randomArray = chosenOptions[Math.floor(Math.random() * Math.floor(chosenOptions.length))]
-      // Pick a random character of that type of array.
-      let randomSelectionOfRandArr = randomArray[Math.floor(Math.random() * Math.floor(randomArray.length))]
-      // Append to password array.
-      passwordArr.push(randomSelectionOfRandArr)
-      
+    // Pick a random array containing a rule the user chose. 
+    let randomArray = chosenOptions[Math.floor(Math.random() * Math.floor(chosenOptions.length))]
+    // Pick a random character of that type of array.
+    let randomSelectionOfRandArr = randomArray[Math.floor(Math.random() * Math.floor(randomArray.length))]
+    // Append to password array.
+    passwordArr.push(randomSelectionOfRandArr)
+
   }
 
+  // Collapses what the characters from an array to strings. 
   let passwordString = passwordArr.join("")
-  console.log(passwordString);
+
+  //console.log(passwordString);
+  return passwordString
 }
 
+// Prompts to choose length.
 function chooseLength() {
   let length = prompt("Choose a length for your password between 8 and 128 characters.")
-  if ((length < 8) || (length > 128))  { //
+  if ((length < 8) || (length > 128)) { //
     alert("You have to pick a number between 8 and 128!")
     chooseLength()
   }
-  else {return length}
+  else { return length }
 }
 
+// Prompts to choose if they want to include lower cases.
 function chooseLowercase() {
   let lowercase = confirm("Would you like lower cases in your password?")
   if (lowercase) {
     return true
   }
-  else {return false}
+  else { return false }
 }
 
+// Prompts to choose if they want to include upper cases.
 function chooseUppercase() {
   let uppercase = confirm("Would you like upper cases in your password?")
   if (uppercase) {
     return true
   }
-  else {return false}
+  else { return false }
 }
 
+// Prompts to choose if they want to include numbers.
 function chooseNumeric() {
   let numeric = confirm("Would you like numbers in your password?")
   if (numeric) {
     return true
   }
-  else {return false}
+  else { return false }
 }
 
+// Prompts to choose if they want to include special characters.
 function chooseSpecialChars() {
   let specialChars = confirm("Would you like special characters in your password?")
   if (specialChars) {
     return true
   }
-  else {return false}
+  else { return false }
 }
 
 
